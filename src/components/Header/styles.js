c8f3def21b57e241eb5f2ea39c6bf588e0ea4e69
Pedro Oliveira <pedro.ott@hotmail.com>
2020-04-30 11:27:22 -0300

    Aula dia 03/04 - Interface inicial do Nubank

---
+++ src/components/Header/styles.js
@@ -0,0 +1,28 @@
+import {StyleSheet} from 'react-native';
+
+export default styles = StyleSheet.create({
+
+     container: {
+        alignItems: 'center',
+        paddingTop: 40,
+        paddingHorizontal: 0,
+        paddingBottom: 0,
+     },
+
+     top: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 10,
+     },
+
+     logo: {
+         
+     },
+
+     title: {
+         fontSize: 18,
+         color: "#fff",
+         fontWeight: 'bold',
+         marginLeft: 8,
+     },
+})
---
+++ src/components/Tabs/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './styles'
+import {View, Text, ScrollView} from 'react-native';
+import Icon from '@expo/vector-icons/MaterialIcons'
+
+export default function Tabs(){
+    return (
+        <View style={styles.container}>
+            <ScrollView
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={styles.tabsContainer}
+            >
+                <View style={styles.tabItem}> 
+                    <Icon name='person-add' size={24} color='#FFF'/>
+                    <Text style={styles.tabText}>Indicar Amigos</Text>
+                </View>
+                <View style={styles.tabItem}>
+                    <Icon name='chat-bubble-outline' size={24} color='#FFF'/>
+                    <Text style={styles.tabText}>Cobrar</Text>
+                </View>
+                <View style={styles.tabItem}>
+                    <Icon name='arrow-downward' size={24} color='#FFF'/>
+                    <Text style={styles.tabText}>Depositar</Text>
+                </View>
+                <View style={styles.tabItem}>
+                    <Icon name='arrow-upward' size={24} color='#FFF'/>
+                    <Text style={styles.tabText}>Transferir</Text>
+                </View>
+                <View style={styles.tabItem}>
+                    <Icon name='lock' size={24} color='#FFF'/>
+                    <Text style={styles.tabText}>Bloquear Cartão</Text>
+                </View>
+            </ScrollView>
+        </View>
+    );
+}
